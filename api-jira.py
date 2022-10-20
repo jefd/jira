fab5f10b0bbe96ee8b5611ed6bd01745c0b536ea
jef <jef.dodson@jpl.nasa.gov>
2022-10-20 13:07:25 -0700

    Add Jira api test code.

---
+++ api-jira.py
@@ -0,0 +1,89 @@
+import requests
+from datetime import datetime
+import json
+
+'''
+curl -H "Authorization: Bearer MDAzNzcxNDQwMzYxOgM14bEjncM6un808o/n39OKludK" -X GET -H "Content-Type: application/json" https://jira-epic.woc.noaa.gov/rest/api/2/issue/createmeta
+
+curl -u "jef.dodson:wWI00ZRAHD0dWThEcGpd" -X GET -H "Content-Type: application/json" https://jira-epic.woc.noaa.gov/rest/api/2/issue/createmeta
+
+'''
+
+
+# jira personal access token for jef.dodson
+TOKEN = 'MDAzNzcxNDQwMzYxOgM14bEjncM6un808o/n39OKludK'
+
+HEADERS = {'Content-Type': 'application/json',
+           'Authorization': f'Bearer {TOKEN}'
+          }
+
+HEADERS = {'Content-Type': 'application/json',
+           'Accept': 'application/json',
+           'Authorization': f'Bearer {TOKEN}'
+          }
+
+
+# URLs
+BASE = f'https://jira-epic.woc.noaa.gov'
+
+meta = f'{BASE}/rest/api/2/issue/createmeta'
+sd = f'{BASE}/rest/servicedeskapi/servicedesk?start=0&limit=100'
+rt = f'{BASE}/rest/servicedeskapi/servicedesk/1/requesttype'
+
+field = f'{BASE}/rest/servicedeskapi/servicedesk/1/requesttype/14/field'
+
+create = f'{BASE}/rest/servicedeskapi/request'
+
+data = {
+    'serviceDeskId': '1',
+    'requestTypeId': '14',
+    'requestFieldValues': {
+        'summary': 'Test summary',
+        'description': 'Test description'
+    }
+}
+
+data = {
+    'serviceDeskId': '1',
+    'requestTypeId': '14',
+    'requestFieldValues': {
+        'summary': 'Test summary',
+        'description': 'Test description',
+        'components': [
+            {'id': '10302'},
+            {'id': '10303'},
+            ],
+        'priority': {'id': '4'},
+        'customfield_10602': {'id': '10514'},
+    }
+}
+
+
+'''
+https://community.atlassian.com/t5/Jira-questions/Service-Desk-API-Adding-Attachment/qaq-p/1072896
+https://stackoverflow.com/questions/12989442/uploading-multiple-files-using-formdata
+'''
+
+
+def get():
+    url = sd
+    print(url)
+    r = requests.get(url, headers=HEADERS)
+    #content = json.loads(r.content)
+    print(r.status_code)
+    print(r.content)
+
+def post():
+    url = create
+    print(url)
+    r = requests.post(url, headers=HEADERS, json=data)
+    #content = json.loads(r.content)
+    print(r.status_code)
+    print(r.content)
+
+
+
+
+
+get()
+
---
+++ upload.py
@@ -0,0 +1,118 @@
+import requests
+import json
+
+
+# jira personal access token for jef.dodson
+TOKEN = 'MDAzNzcxNDQwMzYxOgM14bEjncM6un808o/n39OKludK'
+
+#url = 'http://localhost:8000'
+BASE = f'https://jira-epic.woc.noaa.gov'
+
+def create_request():
+    create_url = f'{BASE}/rest/servicedeskapi/request'
+    print(create_url)
+
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Authorization': f'Bearer {TOKEN}'
+              }
+    data = {
+        'serviceDeskId': '1',
+        'requestTypeId': '14',
+        'requestFieldValues': {
+            'summary': 'Test summary with attachments',
+            'description': 'Test description with attachments',
+            'components': [
+                {'id': '10302'},
+                {'id': '10303'},
+                ],
+            'priority': {'id': '4'},
+            'customfield_10602': {'id': '10514'},
+        }
+    }
+
+    r = requests.post(create_url, headers=headers, json=data)
+    print(r.status_code)
+    print(r.content)
+    if r.status_code == 201:
+        content = json.loads(r.content)
+        #issue_id = content['issueId']
+        return content
+
+
+def upload():
+    upload_url = f'{BASE}/rest/servicedeskapi/servicedesk/1/attachTemporaryFile'
+    print(upload_url)
+
+    headers = {'Accept': 'application/json',
+               'Authorization': f'Bearer {TOKEN}',
+               'X-Atlassian-Token': 'no-check',
+               'X-ExperimentalApi': 'opt-in',
+              }
+
+
+    # files = [('file', open('a.txt', 'rb')), ('file', open('a.html', 'rb')), ('file', open('binary', 'rb'))]
+
+
+    multiple_files = [
+
+        ('file', ('a.txt', open('a.txt', 'rb'), 'text/plain')),
+        ('file', ('a.html', open('a.html', 'rb'), 'text/html')),
+        ('file', ('a.bin', open('a.bin', 'rb'), 'application/octet-stream')),
+    ]
+
+    # r = requests.post(url, data=payload, files=files)
+    r = requests.post(upload_url, headers=headers, files=multiple_files)
+    print(r.status_code)
+    print(r.content)
+    if r.status_code == 201:
+        content = json.loads(r.content)
+        return content
+        
+
+def attach(issue_id, temp_ids):
+    attach_url = f'{BASE}/rest/servicedeskapi/request/{issue_id}/attachment'
+    print(attach_url)
+    #issue_id = '13002'
+    headers = {'Accept': 'application/json',
+               'Content-Type': 'application/json',
+               'Authorization': f'Bearer {TOKEN}',
+               #'X-Atlassian-Token': 'no-check',
+               'X-ExperimentalApi': 'opt-in',
+              }
+
+    '''
+    json = {
+            'temporaryAttachmentIds': [
+                'temp13320094701793446871',
+                'temp18358553896228365731',
+                'temp11794625735693222861',
+            ],
+            'public': True
+    }
+    '''
+
+    json_data = {'temporaryAttachmentIds': temp_ids, 
+                 'public': True}
+            
+
+    r = requests.post(attach_url, headers=headers, json=json_data)
+    print(r.status_code)
+    print(r.content)
+    if r.status_code == 201:
+        content = json.loads(r.content)
+        return content
+
+
+def main():
+    sd_request = create_request()
+    issue_id = sd_request['issueId']
+    content = upload()
+    temp_ids = [item['temporaryAttachmentId'] for item in content['temporaryAttachments']] 
+    attach(issue_id, temp_ids)
+     
+
+if __name__ == '__main__':
+    main()
+
+
